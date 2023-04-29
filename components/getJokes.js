6880dda5114eef91c37eb0cf73906396a3e4d1fa
Angel David Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2023-04-29 10:08:36 -0500

    feat: :sparkles: Introduce the random joke
    Using async funtions, propertys of the api and more

--- components/getJokes.js
+++ components/getJokes.js
@@ -1,5 +1,17 @@
 export default {
     getJokes() {
-        
+
+        const joke = document.querySelector('#joke');
+        const btn = document.querySelector('#btn');
+
+        let getJoke = async () => {
+            const urlAPI = await fetch("https://v2.jokeapi.dev/joke/Any");
+            const responseJson = await urlAPI.json();
+
+            joke.textContent = responseJson.joke || `${responseJson.setup} ${responseJson.delivery}`;
+        }
+
+        btn.addEventListener('click', getJoke)
+
     }
 }
